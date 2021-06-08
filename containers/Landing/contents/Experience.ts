db77fe3b5aabe81e8b9eb90393284b6f25c957af
0xbono <iam@sonjim.in>
2021-06-09 00:26:00 +0900

    chore(containers/landing): Work Experience 데이터 콘텐츠 추가

---
+++ containers/Landing/contents/Experience.ts
@@ -0,0 +1,29 @@
+export const Experience = [
+  {
+    content: {
+      tag: ['팀빌딩'],
+      img: '/assets/beginmate_logo.svg',
+      title: '비긴메이트',
+      date: new Date('November 29, 2019'),
+    },
+    to: 'https://okkot.com/',
+  },
+  {
+    content: {
+      tag: ['이커머스'],
+      img: '/assets/okkot_logo.svg',
+      title: '오늘의꽃',
+      date: new Date('December 1, 2020'),
+    },
+    to: 'https://okkot.com/',
+  },
+  {
+    content: {
+      tag: ['프로그램'],
+      img: '/assets/plask_logo.svg',
+      title: '플라스크',
+      current: true,
+    },
+    to: 'https://plask.ai/',
+  },
+];
