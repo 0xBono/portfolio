d5632fe4541563fa32fde79df3f2a09d14e81db2
0xbono <iam@sonjim.in>
2021-06-09 02:39:09 +0900

    feat(containers/landing): 랜딩 페이지에 어워드 추가

--- containers/Landing/Landing.tsx
+++ containers/Landing/Landing.tsx
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import { GNBLayout } from 'components/Layout';
 import { Jumbotron } from 'components/Jumbotron';
-import { ImageCard } from 'components/Card';
-import { Experience } from './contents';
+import { ImageCard, AwardCard } from 'components/Card';
+import { Experience, FavoriteAward, Award } from './contents';
 
 import classNames from 'classnames/bind';
 import styles from './Landing.module.scss';
@@ -18,7 +18,7 @@ export const Landing: React.FC = () => {
         <div className={cx('card-content')}>
           {_.map(Experience, (item, idx) => (
             <ImageCard
-              articleId={idx.toString()}
+              id={idx.toString()}
               content={{
                 img: item.content.img,
                 tag: item.content.tag,
@@ -27,6 +27,34 @@ export const Landing: React.FC = () => {
                 current: item.content.current,
               }}
               to={item.to}
+            />
+          ))}
+        </div>
+      </section>
+      <section className={cx('work-section')}>
+        <span className={cx('content-title')}>Award</span>
+        <div className={cx('card-content')}>
+          {_.map(FavoriteAward, (item, idx) => (
+            <ImageCard
+              id={idx.toString()}
+              content={{
+                img: item.content.img,
+                tag: item.content.tag,
+                title: item.content.title,
+                date: item.content.date,
+                current: item.content.current,
+              }}
+              to={item.to}
+            />
+          ))}
+          {_.map(Award, (item, idx) => (
+            <AwardCard
+              id={idx.toString()}
+              content={{
+                tag: item.content.tag,
+                title: item.content.title,
+                date: item.content.date,
+              }}
             />
           ))}
         </div>
