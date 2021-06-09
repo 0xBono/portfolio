898feb46fedad1ac5b009a3ccc785c6ec4385007
0xbono <iam@sonjim.in>
2021-06-10 03:12:14 +0900

    feat(components/icons): ArrowLeft, Right 아이콘 추가

--- components/Icons/SvgPath.ts
+++ components/Icons/SvgPath.ts
@@ -2,8 +2,10 @@ import { FunctionComponent } from 'react';
 import BlueCircle from './svgs/BlueCircle.svg';
 import Scribble from './svgs/Scribble.svg';
 import ArrowDown from './svgs/ArrowDown.svg';
+import ArrowRight from './svgs/ArrowRight.svg';
+import ArrowLeft from './svgs/ArrowLeft.svg';
 
-type Icon = 'BlueCircle' | 'Scribble' | 'ArrowDown';
+type Icon = 'BlueCircle' | 'Scribble' | 'ArrowDown' | 'ArrowRight' | 'ArrowLeft';
 
 type Images = {
   [key in Icon]: FunctionComponent;
@@ -13,6 +15,8 @@ const images: Images = {
   BlueCircle,
   Scribble,
   ArrowDown,
+  ArrowRight,
+  ArrowLeft,
 };
 
 export default images;
