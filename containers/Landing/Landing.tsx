eb5dab86b812bd5148e4bb6f7dd0245e3a7c9f9c
0xbono <iam@sonjim.in>
2021-06-10 03:20:55 +0900

    feat(containers/landing): 어워드 carousl 기능 추가

--- containers/Landing/Landing.tsx
+++ containers/Landing/Landing.tsx
@@ -1,8 +1,10 @@
 import _ from 'lodash';
+import { useRef } from 'react';
 import { GNBLayout } from 'components/Layout';
 import { Jumbotron } from 'components/Jumbotron';
-import { ImageCard, AwardCard } from 'components/Card';
-import { Experience, FavoriteAward, Award } from './contents';
+import { ImageCard } from 'components/Card';
+import { Icon, SvgPath } from 'components/Icons';
+import { Experience, Award } from './contents';
 
 import classNames from 'classnames/bind';
 import styles from './Landing.module.scss';
@@ -10,6 +12,27 @@ import styles from './Landing.module.scss';
 const cx = classNames.bind(styles);
 
 export const Landing: React.FC = () => {
+  const carouslRef = useRef<HTMLDivElement>(null);
+
+  const handlePrev = () => {
+    carouslRef &&
+      carouslRef.current &&
+      carouslRef.current.scrollTo({
+        behavior: 'smooth',
+        top: 0,
+        left: carouslRef.current.scrollLeft - carouslRef.current.clientWidth * 0.32,
+      });
+  };
+
+  const handleNext = () => {
+    carouslRef &&
+      carouslRef.current &&
+      carouslRef.current.scrollTo({
+        behavior: 'smooth',
+        top: 0,
+        left: carouslRef.current.scrollLeft + carouslRef.current.clientWidth * 0.32,
+      });
+  };
   return (
     <GNBLayout>
       <Jumbotron />
@@ -33,8 +56,12 @@ export const Landing: React.FC = () => {
       </section>
       <section className={cx('work-section')}>
         <span className={cx('content-title')}>Award</span>
-        <div className={cx('card-content')}>
-          {_.map(FavoriteAward, (item, idx) => (
+        <div className={cx('carousl-button')}>
+          <Icon className={cx('icon')} icon={SvgPath.ArrowLeft} onClick={handlePrev} />
+          <Icon className={cx('icon')} icon={SvgPath.ArrowRight} onClick={handleNext} />
+        </div>
+        <div ref={carouslRef} className={cx('slide-content')}>
+          {_.map(Award, (item, idx) => (
             <ImageCard
               id={idx.toString()}
               content={{
@@ -42,19 +69,8 @@ export const Landing: React.FC = () => {
                 tag: item.content.tag,
                 title: item.content.title,
                 date: item.content.date,
-                current: item.content.current,
               }}
               to={item.to}
-            />
-          ))}
-          {_.map(Award, (item, idx) => (
-            <AwardCard
-              id={idx.toString()}
-              content={{
-                tag: item.content.tag,
-                title: item.content.title,
-                date: item.content.date,
-              }}
             />
           ))}
         </div>
